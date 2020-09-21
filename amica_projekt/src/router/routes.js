119e0562413ae0e6b60416c388b4f39af729ed79
abbsimoga <43879969+abbsimoga@users.noreply.github.com>
2020-09-21 13:11:47 +0200

    multiple blank pages

--- amica_projekt/src/router/routes.js
+++ amica_projekt/src/router/routes.js
@@ -7,9 +7,17 @@ const routes = [
       { path: '', component: () => import('pages/Index.vue') }
     ]
   },
-
   // Always leave this as last one,
   // but you can also remove it
+  {
+    path: '/Admin',
+    component: () => import('pages/Admin.vue')
+  },
+  {
+    path: '/User',
+    component: () => import('pages/User.vue')
+  },
+
   {
     path: '*',
     component: () => import('pages/Error404.vue')

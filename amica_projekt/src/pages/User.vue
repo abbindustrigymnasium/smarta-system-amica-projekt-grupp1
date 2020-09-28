6aeb9c1b522f2832a6f9a10e38d454427901e6bd
abblinkas <43879956+abblinkas@users.noreply.github.com>
2020-09-28 13:50:47 +0200

    första sidan

--- amica_projekt/src/pages/User.vue
+++ amica_projekt/src/pages/User.vue
@@ -1,14 +1,23 @@
 <template>
-  <div class="q-pa-md q-gutter-md">
+  <div>
     <div class="row">
-      <div class="col-4"></div>
-      <div class="col-4">
-        <q-btn round><q-avatar size="100px"><img src="https://cdn.quasar.dev/img/avatar2.jpg"></q-avatar></q-btn>
-        <q-btn round><q-avatar size="100px"><img src="https://cdn.quasar.dev/img/avatar2.jpg"></q-avatar></q-btn>
-        <q-btn round><q-avatar size="100px"><img src="https://cdn.quasar.dev/img/avatar2.jpg"></q-avatar></q-btn>
-        <q-btn round><q-avatar size="100px"><img src="https://cdn.quasar.dev/img/avatar2.jpg"></q-avatar></q-btn>
+      <div class="col">
       </div>
-      <div class="col-4"></div>
+      <div class="col-7 col-md-auto" style="text-align: center">
+        <h2 style="height: 15vh">Vad åt du idag?</h2>
+      </div>
+      <div class="col">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-6">
+        <q-btn flat style="color: #000000; width: 100%; height: 35vh" size="24px" label="Spaghetti och köttfärsås" />
+        <q-btn flat style="color: #000000; width: 100%; height: 35vh" size="24px" label="Kötbullar" />
+      </div>
+      <div class="col-6">
+        <q-btn flat style="color: #000000; width: 100%; height: 35vh" size="24px" label="Pizza" />
+        <q-btn flat style="color: #000000; width: 100%; height: 35vh" size="24px" label="Soppa" />
+      </div>
     </div>
   </div>
 </template>

6857b8ba7fb111537b27a0eb0f32ed3ae5e635c3
abbkhepho <43946998+abbkhepho@users.noreply.github.com>
2020-10-09 14:50:28 +0200

    brian sa till mig

--- amica_projekt/src/pages/Request.vue
+++ amica_projekt/src/pages/Request.vue
@@ -1,11 +1,29 @@
 <template>
   <div class="flex flex-center">
-      lkkldkla
+    <p v-html="info"></p>
   </div>
 </template>
 
 <script>
-export default {
+const axios = require('axios').default
 
+export default {
+  name: 'app',
+  data () {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    var today = new Date()
+    var dd = String(today.getDate()).padStart(2, '0')
+    var mm = String(today.getMonth() + 1).padStart(2, '0')
+    var yyyy = today.getFullYear()
+    today = yyyy + '-' + mm + '-' + dd
+
+    axios
+      .get('https://cors-anywhere.herokuapp.com/https://foodandco.se/api/restaurant/menu/day?date=' + today + '&language=sv&restaurantPageId=188244')
+      .then(response => (this.info = response.data.LunchMenu.Html))
+  }
 }
 </script>

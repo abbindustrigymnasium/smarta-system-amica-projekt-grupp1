d7182b7f1834a7ee2088887c3729e6acf73ac9ca
abblinkas <43879956+abblinkas@users.noreply.github.com>
2020-10-13 12:39:55 +0200

    update

--- amica_projekt/src/pages/User.vue
+++ amica_projekt/src/pages/User.vue
@@ -1,78 +1,50 @@
 <template>
-  <div class="bg-darkBackground">
-    <div class="row">
-      <div class="col"/>
-      <div class="col-7 col-md-auto" style="text-align: center">
-        <h2 class="text-deep-purple-4" style="height: 15vh">Vad åt du idag?</h2>
+  <div>
+    <div class="bg-darkBackground window-height window-width justify-center items-center">
+      <div class="column" style="height: 20%">
+        <div class="col-8" style="text-align: center">
+          <h2 class="text-deep-purple-4">Vad åt du idag?</h2>
+        </div>
       </div>
-      <div class="col"/>
-    </div>
-    <div class="row justify-around">
-      <div class="col-6">
-        <q-btn class="bg-darkBackgroundLayer" text-color="teal-14" flat style="width: 100%; height: 35vh" size="24px" :label="mat1" @click=toggleDialog(1,mat1) />
-        <q-btn class="bg-darkBackgroundLayer" text-color="teal-14" flat style="width: 100%; height: 35vh" size="24px" label="Kötbullar" @click=toggleDialog(1,mat2) />
-      </div>
-      <div class="col-6">
-        <q-btn class="bg-darkBackgroundLayer" text-color="teal-14" flat style="width: 100%; height: 35vh" size="24px" label="Pizza" @click=toggleDialog(1,mat3) />
-        <q-btn class="bg-darkBackgroundLayer" text-color="teal-14" flat style="width: 100%; height: 35vh" size="24px" label="Soppa" @click=toggleDialog(1,mat4) />
+      <div class="column" style="height: 80%">
+        <div class="col-6">
+          <q-btn class="bg-darkBackgroundLayer" text-color="teal-14" style="width: 50%; height: 100%" size="24px" :label="mat1" @click=toggleDialog(1,mat1) />
+          <q-btn class="bg-darkBackgroundLayer" text-color="teal-14" style="width: 50%; height: 100%" size="24px" :label="mat2" @click=toggleDialog(1,mat2) />
+        </div>
+        <div class="col-6">
+          <q-btn class="bg-darkBackgroundLayer" text-color="teal-14" style="width: 50%;  height: 100%" size="24px" :label="mat3" @click=toggleDialog(1,mat3) />
+          <q-btn class="bg-darkBackgroundLayer" text-color="teal-14" style="width: 50%;  height: 100%" size="24px" :label="mat4" @click=toggleDialog(1,mat4) />
+        </div>
       </div>
     </div>
     <div>
-      <q-dialog v-model="dialog1" class="flex flex-center">
-        <q-card class="col-12 col-xl-2 justify-center">
-          <q-card-section class="row justify-center full-height full-width text-center">
-            <h4>Betygsätt {{clicked}}</h4>
-          </q-card-section>
-              <div class="row justify-center full-height full-width text-center">
-                <q-rating
-                          v-model="ratingModel"
-                          size="3.5em"
-                          color="yellow"
-                          icon="star_border"
-                          icon-selected="star"
-                          @click=toggleDialog(2)
-                        />
-          </div>
-          <q-card-section class="row items-center">
-          </q-card-section>
-        </q-card>
+      <q-dialog v-model="dialog1">
+          <q-card style="width: 100%; height: 50%" class="bg-darkBackgroundLayer">
+            <q-card-section class="text-center items-center">
+              <h4 class="text-deep-purple-4">Betygsätt {{clicked}}</h4>
+              <q-rating v-model="ratingModel" size="3.5em" color="teal-14" icon="star_border" icon-selected="star" @click=toggleDialog(2) />
+            </q-card-section>
+          </q-card>
       </q-dialog>
-    </div>
-    <div>
       <q-dialog flat v-model="dialog2">
-        <div>
-          <q-card>
-            <div id="q-app" class="row">
+        <q-card style="width: 100%; height: 50%" class="bg-darkBackgroundLayer">
+          <div class="justify-center items-center">
+            <div>
               <div>
-                <div>
-                  <q-select
-                    filled
-                    v-model="model"
-                    use-input
-                    hide-selected
-                    fill-input
-                    input-debounce="0"
-                    :options="options"
-                    @filter="filterFn"
-                    style="width: 250px"
-                    label = "Föreslå en maträtt"
-                  >
-                    <template v-slot:no-option>
-                      <q-item>
-                        <q-item-section class="text-grey">
-                          No results
-                        </q-item-section>
-                      </q-item>
-                    </template>
-                  </q-select>
-                </div>
+                <q-select filled v-model="model" use-input hide-selected fill-input input-debounce="0" :options="options" @filter="filterFn" label="Föreslå en maträtt" color="teal-14" label-color="teal-14" dark outlined>
+                  <template v-slot:no-option>
+                    <q-item>
+                      <q-item-section class="text-grey">
+                        No results
+                      </q-item-section>
+                    </q-item>
+                  </template>
+                </q-select>
               </div>
             </div>
-          </q-card>
-          <div class="row">
-            <q-btn label="Submit" type="submit" color="primary"/>
           </div>
-        </div>
+          <q-btn size="lg" class="bg-deep-purple-4 absolute-bottom" label="Submit" type="submit" color="primary" style="margin:auto; width:50%"/>
+         </q-card>
       </q-dialog>
     </div>
   </div>

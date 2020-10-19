3f5a85f997fdc98945ae2464e756f0641a0998fb
abblinkas <43879956+abblinkas@users.noreply.github.com>
2020-10-19 14:51:33 +0200

    update

--- amica_projekt/src/pages/User.vue
+++ amica_projekt/src/pages/User.vue
@@ -1,54 +1,54 @@
 <template>
   <div class="bg-darkBackground window-height column">
-      <h2 class="text-deep-purple-4 q-ma-none text-center q-py-xl col-2">Vad åt du idag?</h2>
-      <div class="row col-5">
-        <q-btn v-for="(food, index) in foodList" :key="index" class="col-6 bg-darkBackgroundLayer" text-color="teal-14" style="width: 50%; height: 100%; " size="24px" v-html="decoder(food.foodName)" @click=toggleDialog(1,food.foodName) />
-        <!-- lägg till food.foodName i classen/indexen så vi kan peka mot de i clickUpdate (använd queryselector?) -->
-      </div>
-      <q-dialog v-model="dialog1">
-          <q-card style="width: 100%; height: flex" class="bg-darkBackgroundLayer">
-            <q-card-section class="text-center items-center">
-              <h4 class="text-deep-purple-4">Betygsätt:</h4>
-              <h5 class="text-deep-purple-4" v-html="decoder(clicked)"></h5>
-              <q-rating v-model="ratingModel" size="3.5em" color="teal-14" icon="star_border" icon-selected="star" @click=toggleDialog(2) />
-              <q-btn size="lg" class="bg-deep-purple-4 absolute-bottom" label="Submit" type="submit" color="primary" style="margin:auto; width:50%" @click=clickUpdate() />
-            </q-card-section>
-          </q-card>
-      </q-dialog>
-      <!-- <q-dialog flat v-model="dialog2">
-        <q-card style="width: 100%; height: 50%" class="bg-darkBackgroundLayer">
-          <div class="justify-center items-center">
-            <div>
-              <div>
-                <q-select filled v-model="model" use-input hide-selected fill-input input-debounce="0" :options="options" @filter="filterFn" label="Föreslå en maträtt" color="teal-14" label-color="teal-14" dark outlined>
-                  <template v-slot:no-option>
-                    <q-item>
-                      <q-item-section class="text-grey">
-                        No results
-                      </q-item-section>
-                    </q-item>
-                  </template>
-                </q-select>
-              </div>
-            </div>
-          </div>
-          <q-btn size="lg" class="bg-deep-purple-4 absolute-bottom" label="Submit" type="submit" color="primary" style="margin:auto; width:50%" @click=clickUpdate() />
-         </q-card>
-      </q-dialog> -->
+    <div v-if="$q.screen.lt.md" class="q-mt-lg row justify-center">
+      <q-btn class="bg-darkBackgroundLayer" text-color="teal-14" unelevated label="Admin Login" no-caps @click="routeAdmin"/>
     </div>
+    <div class="row col-2 text-center justify-center">
+      <q-btn v-if="$q.screen.gt.sm" class="q-mt-xl bg-darkBackgroundLayer" style="right: 30px; top: 10px; position: absolute" text-color="teal-14" unelevated label="Admin Login" no-caps @click="routeAdmin"/>
+      <h2 class="text-deep-purple-4 q-ma-none text-center q-py-xl justify-center">Vad åt du idag?</h2>
+    </div>
+    <div class="row col-5">
+      <q-btn v-for="(food, index) in foodList" :key="index" class="col-6 bg-darkBackgroundLayer" text-color="teal-14" style="width: 50%; height: 100%; " size="16px" v-html="decoder(food.foodName)" @click=toggleDialog(1,food.foodName,food.id) />
+    </div>
+    <q-dialog v-model="dialog1">
+      <q-card style="width: 100%; height: flex" class="bg-darkBackgroundLayer">
+        <q-card-section class="text-center items-center">
+          <h4 class="text-red-4" v-if="hasVoted || wrongInput">Betygsätt:</h4>
+          <h4 class="text-deep-purple-4" v-else>Betygsätt:</h4>
+          <h5 class="text-red-4" v-if="hasVoted || wrongInput" v-html="decoder(clicked)"></h5>
+          <h5 class="text-deep-purple-4" v-else v-html="decoder(clicked)"></h5>
+
+          <div class="col">
+            <q-rating v-if="hasVoted || wrongInput" v-model="ratingModel" size="3.5em" color="red-10" icon="star_border" icon-selected="star" @click=toggleDialog(2) />
+            <q-rating v-else v-model="ratingModel" size="3.5em" color="teal-14" icon="star_border" icon-selected="star" @click=toggleDialog(2) />
+          </div>
+
+          <div class="col q-mt-md">
+            <q-btn v-if="hasVoted || wrongInput" size="lg" class="col absolute-bottom bg-darkBackgroundLayer2" text-color="red-10" label="Bekräfta" type="submit" style="margin:auto; width:50%" @click="clickUpdate()"/>
+            <q-btn v-else size="lg" class="col absolute-bottom bg-darkBackgroundLayer2" text-color="teal-14" label="Bekräfta" type="submit" style="margin:auto; width:50%" @click="clickUpdate()"/>
+          </div>
+
+          <q-card-section v-if="!wrongInput && hasVoted" class="text-center q-pa-none" style="border-radius: 5px;">
+            <p class="text-red-10">Du har tyvärr redan betygsatt<br>Du kan betygsätta imorgon igen</p>
+          </q-card-section>
+          <q-card-section v-else-if="wrongInput" class="text-center q-pa-none" style="border-radius: 5px;">
+            <p class="text-red-10">Du kan tyvärr inte betygsätta 0 stjärnor<br>Testa att trycka på stjärnorna</p>
+          </q-card-section>
+        </q-card-section>
+      </q-card>
+    </q-dialog>
+  </div>
 </template>
 
 <script>
 import { parseXml, xml2json } from '../xml_to_json'
 import { db, increment } from '../boot/firebase'
-const stringOptions = ['Google', 'Facebook', 'Twitter', 'Apple', 'Oracle']
 export default {
   name: 'User',
   data () {
     return {
       date: null,
       dialog1: false,
-      dialog2: false,
       foodList: {
         foodOne: { foodName: ''},
         foodTwo: { foodName: ''},
@@ -56,22 +56,29 @@ export default {
         foodFour: { foodName: ''}
       },
       clicked: '',
-      options: stringOptions,
       ratingModel: 0,
-      ratingVal: 0, // Stjärnans värde
-      model: '',
-      modelVal: '' // Önskade maträttens värde
+      ratingVal: 0,
+      currentFoodHover: null,
+      hasVoted: false,
+      wrongInput: false
     }
   },
   methods: {
-    toggleDialog: function (state, mat) {
+    routeAdmin: function () {
+      this.$router.push({ path: '/Admin' })
+    },
+    toggleDialog: function (state, mat, foodname) {
       if (state === 1) {
+        this.hasVoted = false
+        this.wrongInput = false
+        this.currentFoodHover = foodname
         console.log(mat)
         this.ratingModel = 0
         this.dialog1 = !this.dialog1
         this.clicked = mat
       }
       if (state === 2) {
+        this.wrongInput = false
         this.ratingVal = this.ratingModel
       }
     },
@@ -121,31 +128,41 @@ export default {
       return textArea.value
     },
     clickUpdate () {
-      var star = this.ratingVal
-      console.log(star)
-      let update
-      switch(star) {
-        case 1:
-          update = {'oneStar': increment}
-          break
-        case 2: 
-          update = {'twoStar': increment}
-          break
-        case 3:
-          update = {'threeStar': increment}
-          break
-        case 4: 
-          update = {'fourStar': increment}
-          break
-        case 5: 
-          update = {'fiveStar': increment}
-          break
+      if (document.cookie !== this.date) {
+        var star = this.ratingVal
+        console.log(star)
+        let update
+        switch(star) {
+          case 0:
+            this.wrongInput = true
+            break
+          case 1:
+            update = {'oneStar': increment}
+            break
+          case 2: 
+            update = {'twoStar': increment}
+            break
+          case 3:
+            update = {'threeStar': increment}
+            break
+          case 4: 
+            update = {'fourStar': increment}
+            break
+          case 5: 
+            update = {'fiveStar': increment}
+            break
+        }
+        if (!this.wrongInput){
+          console.log(this.currentFoodHover)
+          db.collection('Dagens_Maträtter').doc('Amica').collection(this.getDate()).doc(this.currentFoodHover).update(update)
+          this.dialog1 = !this.dialog1
+          document.cookie = this.date
+          this
+        }
+      } else {
+        this.wrongInput = false
+        this.hasVoted = true
       }
-
-      var selectedFood = this.decoder(this.clicked)
-      console.log(selectedFood)
-      db.collection('Dagens_Maträtter').doc('Amica').collection(this.getDate()).doc(selectedFood).update(update)
-      this.dialog2 = !this.dialog2
     },
     getDate () {
       var today = new Date()
@@ -198,5 +215,8 @@ export default {
     position: absolute;
     top: 43%;
     width: 100%;
+  }
+  .bg-darkBackgroundLayer2 {
+    background-color: #302A38
   }
 </style>
